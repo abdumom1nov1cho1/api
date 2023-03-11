b9dc014ba00322635800aa85f45bc53791126cad
abdumom1nov1cho1 <116268072+abdumom1nov1cho1@users.noreply.github.com>
2023-03-11 16:17:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const todosRouter = require('./routes/todos');//esxpress
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const cors = require("cors");
+
+const app = express();//express () bn qoyilwi kere ctobi cistyro bosin!
+
+app.use(cors())
+app.use(express.json());
+app.use('/TrailFX.com', todosRouter);
+
+const PORT = process.env.PORT || 8000;//env port ucun
+
+function start () {
+    try {
+     mongoose.connect(process.env.MONGO_URL, {
+        useNewURLParser: true,
+     });
+     app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+start()
